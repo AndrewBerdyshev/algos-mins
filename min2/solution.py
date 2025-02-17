88e83562839902952b5c93c4415506370ca45da7
Andrew Berdyshev <an.berdyshev@gmail.com>
2025-02-17 16:54:02 +0700

    min2 changes

--- min2/solution.py
+++ min2/solution.py
@@ -20,7 +20,7 @@ def karatsuba(x: int, y: int) -> int:
     return ac * (10 ** (2 * halflen)) + (abcd - ac - bd) * (10 ** halflen) + bd
 
 def test1(capsys):
-    assert karatsuba(1234, 5678) == 7006652
+    assert karatsuba(1234, 5678) == 1234 * 5678
 
 def test2(capsys):
     assert karatsuba(0, 12345) == 0
@@ -32,7 +32,35 @@ def test4(capsys):
     assert karatsuba(1, 1) == 1
 
 def test5(capsys):
-    assert karatsuba(9999, 9999) == 99980001
+    assert karatsuba(9999, 9999) == 9999 * 9999
 
 def test6(capsys):
-    assert karatsuba(123456789, 987654321) == 121932631112635269
+    assert karatsuba(123456789, 987654321) == 123456789 * 987654321
+
+def test7(capsys):
+    assert karatsuba(123456789, 98) == 123456789 * 98
+
+def test8(capsys):
+    assert karatsuba(98, 1234567892323) == 98 * 1234567892323
+
+def test9(capsys):
+    assert karatsuba(9, 123456789232112121212123) == 9 * 123456789232112121212123
+
+# import random
+
+# def python_mult(a: int, b: int) -> int:
+#     return a*b
+
+# def test_mult():
+#     for _ in range(1000):
+#         a = random.randint(1, 10**9)
+#         b = random.randint(1, 10**9)
+        
+#         res1 = karatsuba(a, b)
+#         res2 = python_mult(a, b)
+        
+#         assert res1 == res2, f"Test failed for a={a}, b={b}: expected {res2}, got {res1}"
+    
+#     print("All tests passed!")
+
+# test_mult()
