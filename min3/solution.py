641f4e92ef1b053ee7d9bcef671718cf2b161446
Andrew Berdyshev <an.berdyshev@gmail.com>
2025-02-17 18:16:38 +0700

    min3 changes

--- min3/solution.py
+++ min3/solution.py
@@ -7,23 +7,46 @@ def call(cmd):
     stdout, stderr = process.communicate()
     return stdout, stderr, process.returncode
 
+def checkout(hash):
+    call(f"git checkout {hash}")
+
 def check(cmd):
     return call(cmd)[2]
+
+def getfullhash(hash):
+    return str(call(f"git rev-parse {hash}")[0])[2:-3]
 
 def getprev(hash):
     return str(call(f"git rev-parse {hash}^")[0])[2:][:7]
 
+def getlist(start, end):
+    s = getfullhash(start)
+    res = str(call(f"git rev-list {end}")[0])[2:-3].replace("\\n", " ").split(getfullhash(s))[0].split()
+    res.reverse()
+    res.insert(0, s)
+    return res
+
+def getsep(list, start, end):
+    return list[list.index(start):list.index(end)+1]
+
 def mybisect(repopath, start, end, checkcommand):
     os.chdir(repopath)
-
-    prev = end
-    cur = end
-    while True:
-        call(f"git checkout {cur}")
+    list = getlist(start, end)
+    start = list[0]
+    end = list[-1]
+    while start != end:
+        list = getsep(list, start, end)
+        cur = list[int(len(list)/2)]
+        checkout(cur)
         if check(checkcommand) == 0:
-            return prev
-        prev = cur
-        cur = getprev(prev)
+            end = cur
+        else:
+            start = cur
+    checkout(start)
+    return start if check(checkcommand) == 1 else None
+
+# bad = mybisect("C:\\Users\\anber\\syspro\\git-bisect-test-repo", "6747dbf", "d0dce63", "python -m pytest main.py")
+# print(f"{bad} is bad")
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
