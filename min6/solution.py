6485bbc382b7e3fa11f775625870a9100bbdd603
Andrew Berdyshev <an.berdyshev@gmail.com>
2025-02-22 21:06:36 +0700

    merge sorst is inplace now :)

--- min6/solution.py
+++ min6/solution.py
@@ -1,44 +1,33 @@
-def merge(left, right, res):
-    lsize, rsize = len(left), len(right)
-    n = len(res)
-    assert n == lsize + rsize
-    i, j, k = 0, 0, 0
-    while k < n and i < lsize and j < rsize:
-        if left[i] < right[j]:
-            res[k] = left[i]
-            k+=1
-            i+=1
+def merge(array, start, mid, end):
+    left = start
+    right = mid + 1
+    while left <= mid and right <= end:
+        if array[left] <= array[right]:
+            left += 1
         else:
-            res[k] = right[j]
-            k+=1
-            j+=1
+            preserve = array[right]
+            for i in range(right, left, -1):
+                array[i] = array[i - 1]
+            array[left] = preserve
+            left += 1
+            mid += 1
+            right += 1
 
-    while i < lsize:
-        res[k] = left[i]
-        k+=1
-        i+=1
-    while j < rsize:
-        res[k] = right[j]
-        k+=1
-        j+=1
-
+def merge_sort_args(array, start, end):
+    if start < end:
+        mid = (start + end) // 2
+        merge_sort_args(array, start, mid)
+        merge_sort_args(array, mid + 1, end)
+        merge(array, start, mid, end)
+    
 def merge_sort(array):
-    if len(array) <= 1:
-        return
-    middle = len(array) // 2
-    left = array[:middle]
-    right = array[middle:]
-    
-    merge_sort(left)
-    merge_sort(right)
-    
-    buffer = [0]*len(array)
-    merge(left, right, buffer)
-    
-    for i in range(len(array)):
-        array[i] = buffer[i]
+    merge_sort_args(array, 0, len(array) - 1)
 
-
-lololo = [1, 2, 3, 7, 6, 5, 3, 4]
-merge_sort(lololo)
-print(lololo)
+import random
+for _ in range(1000):
+    unsorted = [random.randint(-20, 20) for __ in range(random.randint(1, 20))]
+    python_sorted = sorted(unsorted.copy())
+    my_sorted = unsorted.copy()
+    merge_sort(my_sorted)
+    assert python_sorted == my_sorted, f"Expected {python_sorted} but got {my_sorted}"
+print("All tests passed!")
