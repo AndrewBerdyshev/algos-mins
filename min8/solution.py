1c42f6d4337f918796d9e23081fd025b9dc94d88
AndrewBerdyshev <an.berdyshev@gmail.com>
2025-02-25 17:17:34 +0700

    strass added

--- min8/solution.py
+++ min8/solution.py
@@ -45,16 +45,16 @@ def recursive_multiply(a, b): # 2
         return multiply(a, b)
     n = ln // 2
     m = ln - 1
+
     A = get_part(a, [0, 0], [n-1, n-1])
     B = get_part(a, [0, n], [n-1, m])
     C = get_part(a, [n, 0], [m, n-1])
     D = get_part(a, [n, n], [m, m])
-    #print(A, B, C, D)
     E = get_part(b, [0, 0], [n-1, n-1])
     F = get_part(b, [0, n], [n-1, m])
     G = get_part(b, [n, 0], [m, n-1])
     H = get_part(b, [n, n], [m, m])
-    #print(E, F, G, H)
+
     res = get_zero_matrixnn(ln)
     set_part(sum(multiply(A, E), multiply(B, G)), [0, 0], [n-1, n-1], res)
     set_part(sum(multiply(A, F), multiply(B, H)), [0, n], [n-1, m], res)
@@ -67,9 +67,44 @@ def recursive_multiply(a, b): # 2
 # [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]]))
 
 def neg(a):
+    res = get_zero_matrixnn(len(a))
     for i in range(len(a)):
         for j in range(len(a[0])):
-            a[i][j] = -a[i][j]
+            res[i][j] = -a[i][j]
+    return res
 
 def strassen(a, b):
-    pass
+    ln = len(a)
+    if ln == 2:
+        return multiply(a, b)
+    n = ln // 2
+    m = ln - 1
+
+    A = get_part(a, [0, 0], [n-1, n-1])
+    B = get_part(a, [0, n], [n-1, m])
+    C = get_part(a, [n, 0], [m, n-1])
+    D = get_part(a, [n, n], [m, m])
+    E = get_part(b, [0, 0], [n-1, n-1])
+    F = get_part(b, [0, n], [n-1, m])
+    G = get_part(b, [n, 0], [m, n-1])
+    H = get_part(b, [n, n], [m, m])
+
+    P1 = strassen(A, sum(F, neg(H)))
+    P2 = strassen(sum(A, B), H)
+    P3 = strassen(sum(C, D), E)
+    P4 = strassen(D, sum(G, neg(E)))
+    P5 = strassen(sum(A, D), sum(E, H))
+    P6 = strassen(sum(B, neg(D)), sum(G, H))
+    P7 = strassen(sum(A, neg(C)), sum(E, F))
+
+    Q1 = sum(sum(sum(P5, P4), neg(P2)), P6)
+    Q2 = sum(P1, P2)
+    Q3 = sum(P3, P4)
+    Q4 = sum(sum(sum(P1, P5), neg(P3)), neg(P7))
+
+    res = get_zero_matrixnn(ln)
+    set_part(Q1, [0, 0], [n-1, n-1], res)
+    set_part(Q2, [0, n], [n-1, m], res)
+    set_part(Q3, [n, 0], [m, n-1], res)
+    set_part(Q4, [n, n], [m, m], res)
+    return res
